2f375a5976af9b144eaf40207a7805fafa3f72d5
Samantha Rand <samantharand@Samanthas-MacBook-Pro-2.local>
2020-04-25 07:55:26 -0500

    art index route lists all art in database

--- resources/art.py
+++ resources/art.py
@@ -1,23 +1,36 @@
 import models
 from flask import Blueprint, request, jsonify
+from playhouse.shortcuts import model_to_dict
 
 art = Blueprint('art', 'art')
 
 @art.route('/', methods=['GET'])
 def art_index():
-	return 'art index reached'
+	result = models.Art.select().dicts()
+	return jsonify(
+		data = [art for art in result],
+		message = 'Found all art in database',
+		status = 200
+	)
 
 @art.route('/', methods=['POST'])
 def create_art():
-	
 	payload = request.get_json()
-	print(payload)
+	# print(payload)
 	new_art = models.Art.create(
 		name=payload['name'],
 		artist=payload['artist'],
 		year_made=payload['year_made'],
 		current_residence=payload['current_residence'],
 	)
-	print(new_art)
+	# print(new_art)
+	art_dict = model_to_dict(new_art)
+	print("")
+	print("art_dict")
+	print(art_dict)
 
-	return 'create art route'
+	return jsonify(
+		data = art_dict,
+		message = f"Successfully added '{art_dict['name']}' to database",
+		status = 201
+		), 201
