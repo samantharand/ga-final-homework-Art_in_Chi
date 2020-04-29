f2ab174b14f1a4e3344b9ea0169d191a949919e7
Samantha Rand <samantharand@Samanthas-MacBook-Pro-2.local>
2020-04-28 19:58:19 -0500

    fixed register bug (thanks izzy)'

--- resources/museum.py
+++ resources/museum.py
@@ -20,11 +20,11 @@ def register():
 	payload['email'] = payload['email'].lower()
 
 	try:
-		models.Museum.get(models.Museum['email'] == payload['email'])
+		models.Museum.get(models.Museum.email == payload['email'])
 		
-		return jsonify (
+		return jsonify(
 			data={},
-			message=f'{payload["email"]} is already registered',
+			message=f' is already registered',
 			status=401
 		), 401
 	
@@ -32,11 +32,11 @@ def register():
 
 		try:
 			# name of museum
-			models.Museum.get(models.Museum['name'] == payload['name'])
+			models.Museum.get(models.Museum.name == payload['name'])
 
-			return jsonify (
+			return jsonify(
 				data={},
-				message=f'{payload["name"]} is already registered',
+				message=f' is already registered',
 				status=401
 			), 401
 
@@ -53,14 +53,63 @@ def register():
 			created_museum_dict = model_to_dict(created_museum)
 			created_museum_dict.pop('password')
 			print(created_museum_dict)
-			return jsonify (
+			return jsonify(
 				data=created_museum_dict,
 				message=f'successfully created {created_museum_dict["name"]}',
 				status=201
 			), 201
 
+# login
+@museum.route('/login', methods=['POST'])
+def login():
+	payload = request.get_json()
+	payload['name'] = payload['name'].lower()
+	payload['email'] = payload['email'].lower()
 
-	#print('request in register', request.get_json())
+	try:
+
+		museum = models.Museum.get(models.Museum.email == payload['email'])
+
+		museum_dict = model_to_dict(museum)
+
+		password_is_good = check_password_hash(museum_dict['password'])
 
 
-	return "check terminal"
+		if password_is_good:
+			login_user(museum)
+			museum_dict.pop('password')
+
+			return jsonify(
+				data=museum_dict,
+				message=f"successfully logged into {museum_dict['name']}'s account",
+				status=200
+			), 200
+	except models.DoesNotExist:
+		print('username doesnt exist, dummy')
+
+		return jsonify(
+			data={},
+			message=f'email or password incorrect :(',
+			status=401
+		), 401
+
+	print(payload)
+	return "checka da termeenal"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
